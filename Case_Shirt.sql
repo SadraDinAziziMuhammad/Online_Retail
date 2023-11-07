3f58c8ccf517ace78b39284c115eff44d11b04b6
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2023-11-07 20:26:56 +0700

    Change Data and Add More Data

--- Case_Shirt.sql
+++ Case_Shirt.sql
@@ -1,8 +1,13 @@
 INSERT INTO Long_Pants (shirt_type, shirt_size, price)
 VALUES
-('Type A', '33', '150000'),
-('Type B', '32', '150000'),
-('Type C', '34', '150000'),
-('Type D', '28', '150000'),
-('Type E', '29', '150000'),
+('Type A', 'M', '175000'),
+('Type B', 'X', '185000'),
+('Type C', 'XXL', '200000'),
+('Type D', 'L', '100000'),
+('Type E', 'S', '350000'),
+('Type F', 'M', '200000'),
+('Type G', 'XX', '165000'),
+('Type H', 'S', '150000'),
+('Type I', 'XL', '130000'),
+('Type J', 'XXXL', '120000'),
 ;

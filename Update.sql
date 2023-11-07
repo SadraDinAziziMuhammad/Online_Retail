2a20fc4f8ac594e8ac362a1495edfc6b61f68657
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2023-11-07 21:01:35 +0700

    Create Query Update

---
+++ Update.sql
@@ -0,0 +1,11 @@
+UPDATE Customers
+SET phone_number = '0895181876205'
+WHERE product_id = 1;
+
+UPDATE Long_Pants
+SET shirt_size = '40'
+WHERE product_id = 4;
+
+UPDATE Shirt
+SET price = '230000'
+WHERE product_id = 9;

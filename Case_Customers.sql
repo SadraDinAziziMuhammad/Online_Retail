dc4087f71e6e0a8bfa0430ba926204309cbe827b
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2023-11-07 20:31:52 +0700

    Change data and Add More Data

--- Case_Customers.sql
+++ Case_Customers.sql
@@ -1,8 +1,12 @@
 INSERT INTO Customers (customer_name, email, phone_number)
 VALUES
-('Sadra', 'sadra@gmail.com', '0812121212'),
-('Regita', 'regita@gmail.com', '0834343434'),
-('Naufal', 'naufal@gmail.com', '0856565656'),
-('Jorghi', 'jorghi@gmail.com', '0878787878'),
-('Reva', 'reva@gmail.com', '0890909090'),
+('Sadra', 'sadra@gmail.com', '0895366976026'),
+('Regita', 'regita@gmail.com', '085810626104'),
+('Naufal', 'naufal@gmail.com', '085878563480'),
+('Jorghi', 'jorghi@gmail.com', '08811246420'),
+('Reva', 'reva@gmail.com', '081317661860'),
+('Navira', 'naira@gmail.com', '0895415546031'),
+('Reysita', 'reysita@gmail.com', '085701143117'),
+('Farisa', 'farisa@gmail.com', '089612121488'),
+('Ndaru', 'ndaru@gmail.com', '0895429766161'),
 ;

49743aa42901ed20d485d5980ba64f56fdc0affd
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2023-11-07 20:17:58 +0700

    Create Case Shirt

---
+++ Case_Shirt.sql
@@ -0,0 +1,8 @@
+INSERT INTO Long_Pants (shirt_type, shirt_size, price)
+VALUES
+('Type A', '33', '150000'),
+('Type B', '32', '150000'),
+('Type C', '34', '150000'),
+('Type D', '28', '150000'),
+('Type E', '29', '150000'),
+;

5c1158e46acbab57ff3f1ab1f0a5d139afb16706
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2023-11-07 20:01:22 +0700

    Create Long Pants

---
+++ Long_Pants.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Customers (
+    longpants_id INT AUTO_INCREMENT PRIMARY KEY,
+    pants_type VARCHAR(255) NOT NULL,
+    pants_size VARCHAR(255) NOT NULL,
+    price VARCHAR(20) NOT NULL
+);

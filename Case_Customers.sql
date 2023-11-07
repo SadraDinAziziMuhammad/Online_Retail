9e1f46ff2b92ef91d5f1b48a6caca671a8f5ffc1
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2023-11-07 19:57:11 +0700

    Create Case Customers

---
+++ Case_Customers.sql
@@ -0,0 +1,8 @@
+INSERT INTO Customers (customer_name, email, phone_number)
+VALUES
+('Sadra', 'sadra@gmail.com', '0812121212'),
+('Regita', 'regita@gmail.com', '0834343434'),
+('Naufal', 'naufal@gmail.com', '0856565656'),
+('Jorghi', 'jorghi@gmail.com', '0878787878'),
+('Reva', 'reva@gmail.com', '0890909090'),
+;
